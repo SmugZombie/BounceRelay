9e9a04dd8059d0b2d17377cbd0146d80e2a67ff9
ThirdPartyReject <git@r-egli.com>
2015-07-06 18:24:04 -0700

    Update mail.sh

--- mail.sh
+++ mail.sh
@@ -1,1 +1,17 @@
+#!/bin/bash
+# getIP.sh
+# author: ThirdPartyReject
+# date: 07/06/2015
 
+to="ron@bouncerelay.com"
+subject="'Test Message'"
+message="'This is a test message'"
+apiKey="//APIKEYHERE//"
+
+# Do not modify below this line
+# Define the Home Url
+homeUrl="https://bouncerelay.com/api/"
+# Make the cURL call
+curlResult=$(curl -k -s --data "key=$apiKey&subject=$subject&message=$message&email=$to" $homeUrl)
+# Echo the result
+echo $curlResult
