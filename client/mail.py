48cc2c21e3771ba4d8a9e2951f1c3e07aaac7116
Ron Egli <ron@r-egli.com>
2016-07-05 16:25:08 -0700

    Create mail.py

---
+++ client/mail.py
@@ -0,0 +1,20 @@
+#/usr/bin/python
+# mail.py
+# github.com/smugzombie/bouncerelay
+
+import requests, commands, HTMLParser, json
+
+def sendMessage(to,subject,message):
+        url = "https://bouncerelay.com/api/1.0/sendmail.json"
+        html = HTMLParser.HTMLParser() # Start HTML Parser
+        payload = {}
+        payload['apikey'] = ""
+        payload['message'] = str(html.unescape(message))
+        payload['subject'] = str(html.unescape(subject))
+        payload['to'] = str(to)
+        headers = {'content-type': "application/json", 'cache-control': "no-cache"}
+        response = requests.request("POST", url, data=json.dumps(payload), headers=headers)
+        print response
+
+data = commands.getstatusoutput("/usr/bin/python /root/elastichealth.py")[1]
+sendMessage("helpdesk@XXXXXXX.com","ElasticSearch Usage",data)
